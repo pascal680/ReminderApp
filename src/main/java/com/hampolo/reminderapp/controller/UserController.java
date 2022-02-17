de7bbcac481e6fc9112a0c4af5c4b558c6430f76
pascal680 <pascal680@hotmail.ca>
2022-02-16 22:31:49 -0500

    Required springboot security configs to work

--- src/main/java/com/hampolo/reminderapp/controller/UserController.java
+++ src/main/java/com/hampolo/reminderapp/controller/UserController.java
@@ -3,7 +3,6 @@ package com.hampolo.reminderapp.controller;
 import com.hampolo.reminderapp.model.User;
 import com.hampolo.reminderapp.repository.UserRepository;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-  @Autowired
   private UserRepository userRepository;
 
-  @GetMapping("/getAll")
+  public UserController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @GetMapping("/all")
   public List<User> getAll(){
     return userRepository.findAll();
   }
