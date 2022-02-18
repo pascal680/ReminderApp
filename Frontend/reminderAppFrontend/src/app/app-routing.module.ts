e846c95cf4154928d94660bad784473c4e766378
pascal680 <pascal680@hotmail.ca>
2022-02-18 11:13:44 -0500

    Completed all base templates for website

--- Frontend/reminderAppFrontend/src/app/app-routing.module.ts
+++ Frontend/reminderAppFrontend/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {ReminderListComponent} from "./components/reminder/reminder-list/reminder-list.component";
 import {ReminderDetailsComponent} from "./components/reminder/reminder-details/reminder-details.component";
+import {CreateReminderComponent} from "./components/reminder/create-reminder/create-reminder.component";
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
   {path:"home", component:ReminderListComponent},
-  {path:"reminder", component: ReminderDetailsComponent}
+  {path:"reminder", component: ReminderDetailsComponent},
+  {path:"createReminder", component: CreateReminderComponent}
+
 ];
 
 @NgModule({
