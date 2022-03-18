0e6d640bd4f6a399433faf1340e3cac2687a2bc3
pascal680 <pascal680@hotmail.ca>
2022-03-18 01:17:08 -0400

    Added logged in validation for header

--- Frontend/reminderAppFrontend/src/app/components/header/header.component.ts
+++ Frontend/reminderAppFrontend/src/app/components/header/header.component.ts
@@ -8,7 +8,7 @@ import {UserService} from "../../services/user-service";
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private userService: UserService) {
+  constructor(public userService: UserService) {
   }
 
   ngOnInit(): void {
